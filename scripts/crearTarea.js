9ae62caaf08af5ee140958a7a35341524713883d
LordShadow98 <juliancodigoc13@gmail.com>
2023-08-30 22:51:23 -0500

    invocando dessde htme js elimnar Tarea

--- scripts/crearTarea.js
+++ scripts/crearTarea.js
@@ -1,36 +1,33 @@
 // Codigo DOM #1
 let arregloTareas = localStorage.getItem('items')
-? JSON.parse(localStorage.getItem('items'))
-: []
- document.querySelector('.new-todo').addEventListener('keyup', (event) => {
+  ? JSON.parse(localStorage.getItem('items'))
+  : [];
+
+document.querySelector('.new-todo').addEventListener('keyup', (event) => {
   if (
     event.keyCode === 13 &&
     document.querySelector('.new-todo').value.length > 0
   ) {
-    const nuevaTarea = document.querySelector('.new-todo')
-    crearTarea(nuevaTarea)    
+    const nuevaTarea = document.querySelector('.new-todo');
+    crearTarea(nuevaTarea);
   }
-  
-})
+});
 
 function crearTarea(nuevaTarea) {
-    nuevaTarea = {
+  nuevaTarea = {
     thing: nuevaTarea.value,
     complete: false,
     dontComplete: true,
     priority: 'Alta',
     category: 'Casa',
-  }
-  arregloTareas.push(nuevaTarea)
-  localStorage.setItem('items', JSON.stringify(arregloTareas))
+  };
+  arregloTareas.push(nuevaTarea);
+  localStorage.setItem('items', JSON.stringify(arregloTareas));
   contadorPendientes();
-  location.reload()
-  
+  location.reload();
 }
-displayFooter()
-displayItems()
 
-function contadorPendientes(){
+function contadorPendientes() {
   const pendingCount = arregloTareas.filter(task => !task.complete).length;
   const countElement = document.querySelector('.todo-count strong');
   countElement.textContent = pendingCount;
@@ -38,4 +35,7 @@ function contadorPendientes(){
 
 window.addEventListener('load', () => {
   contadorPendientes();
-})
+});
+
+displayFooter();
+displayItems();
