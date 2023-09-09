52b7e3b603bed5f62ad0108be75cb4c4f9838213
Monalisa330 <laurameliza26@gmail.com>
2023-09-08 23:26:05 -0500

    Terminando editar tarea

--- scripts/editarTarea.js
+++ scripts/editarTarea.js
@@ -1,71 +1,75 @@
-function editarTarea(){
-  document.querySelector('.todo-list').addEventListener('click', (event) => {
-    if (event.target.classList.contains('editBtn')){ 
-      activarEdit()
-      activateSaveListeners()
-      activateCancelListeners()
-    }
-  })
- };
+document.querySelector('.todo-list').addEventListener('click', (event) => {
+  if (event.target.classList.contains('editBtn')) {
+    activarEdit();
+    activateCancelListeners();
+    activateSaveListeners();
+  }
+});
+
 const activarEdit = function() {
-  const editarBtn = document.querySelectorAll('.editBtn')
-  const controladorActualizacion = document.querySelectorAll('.update-controller')
-  const input = document.querySelectorAll('.input-controller textarea')
-  const seleccionarPrioridad = document.querySelectorAll(
-    '.edit-controller select'
-  )[0]
-  const seleccionarCategoria = document.querySelectorAll(
-    '.edit-controller select'
-  )[1]
+  const editarBtn = document.querySelectorAll('.editBtn');
+  const controladorActualizacion = document.querySelectorAll('.update-controller');
+  const input = document.querySelectorAll('.input-controller textarea');
+  const seleccionarPrioridad = document.querySelectorAll('.edit-controller select')[0];
+  const seleccionarCategoria = document.querySelectorAll('.edit-controller select')[1];
 
   editarBtn.forEach((eb, i) => {
-    eb.addEventListener('click', () => {
-      controladorActualizacion[i].style.display = 'block'
-      input[i].disabled = false
+      controladorActualizacion[i].style.display = 'block';
+      input[i].disabled = false;
+      seleccionarPrioridad.value = arregloTareas[i].priority;
+      seleccionarCategoria.value = arregloTareas[i].category;
+      
+  });
 
-      seleccionarPrioridad.value = arregloTareas[i].priority
-      seleccionarCategoria.value = arregloTareas[i].category
-    })
-  })
-
-  const prioridad = document.querySelectorAll('#priority')
+  const prioridad = document.querySelectorAll('#priority');
   prioridad.forEach((s, i) => {
     s.addEventListener('change', (event) => {
-      arregloTareas[i].priority = event.target.value
-      localStorage.setItem('items', JSON.stringify(arregloTareas))
-    })
-  })
+      arregloTareas[i].priority = event.target.value;
+      localStorage.setItem('items', JSON.stringify(arregloTareas));
+    });
+  });
 
-  const categoria = document.querySelectorAll('#category')
+  const categoria = document.querySelectorAll('#category');
   categoria.forEach((s, i) => {
     s.addEventListener('change', (event) => {
-      arregloTareas[i].category = event.target.value
-      localStorage.setItem('items', JSON.stringify(arregloTareas))
-    })
-  })
+      arregloTareas[i].category = event.target.value;
+      localStorage.setItem('items', JSON.stringify(arregloTareas));
+    });
+  });
 };
 
-
+// Darle vida a los botones "Guardar" y "Cancelar" para un índice específico
+// Permite que la acción guardar el nuevo nombre de la tarea cuando decides editar y que impacte el DOM del HTML, acá debes agegar algoritmo de actualizar tarea
 function activateSaveListeners() {
-  const guardarBtn = document.querySelectorAll('.saveBtn')
-  const input = document.querySelectorAll('.input-controller textarea')
-  guardarBtn.forEach((gB, i) => {
-    gB.addEventListener('click', () => {
-      updateItem(input[i].value, i)
-    })
-  })
-};
-
-function activateCancelListeners() {
-  const cancelarBtn = document.querySelectorAll('.cancelBtn')
-  const controladorActualizacion = document.querySelectorAll('.update-controller')
+  const saveBtn = document.querySelectorAll('.saveBtn')
   const inputs = document.querySelectorAll('.input-controller textarea')
-  
-  cancelarBtn.forEach((cB, i) => {
-    cB.addEventListener('click', () => {
-      updateController[i].style.display = 'none'
-      inputs[i].disabled = true
-      inputs[i].style.border = 'none'
+  saveBtn.forEach((saveBtn, index) => {
+    saveBtn.addEventListener('click', () => {
+      // Llamar la función que guarda la actualización la tarea
+      console.log(arregloTareas[index].thing)
+      const textareaValue = inputs[index].value;
+      console.log(textareaValue)
+      arregloTareas[index].thing = textareaValue;
+      localStorage.setItem('items',JSON.stringify(arregloTareas))
+      location.reload()
     })
   })
-};
+}
+ 
+function activateCancelListeners() {
+  const cancelarBtn = document.querySelectorAll('.cancelBtn');
+  const controladorActualizacion = document.querySelectorAll('.update-controller');
+  const inputs = document.querySelectorAll('.input-controller textarea');
+
+  cancelarBtn.forEach((btnCancel, index) => {
+    btnCancel.addEventListener('click', () => { 
+      controladorActualizacion[index].style.display = 'none';
+      inputs[index].disabled = true;
+      inputs[index].style.border = 'none';
+      location.reload();
+    });
+  });
+}
+
+// Llamar a la función para activar los listeners de cancelación
+activateCancelListeners();
